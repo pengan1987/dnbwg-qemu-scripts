170e5f94ca782a92b2ad3d4ac2359b415d00c9b8
Andy(Pengan) Zhou <pengan1987@msn.com>
2020-04-07 13:31:48 -0700

    update script

--- startblue.sh
+++ startblue.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+QEMUCMD="qemu"
+
+if type qemu-system-i386 > /dev/null ; then
+ QEMUCMD="qemu-system-i386"
+fi
+echo $QEMUCMD
+
 pkill -f "websockify 45901 localhost:5901"
-pkill -f "qemu-system-i386 -hda bluepoint-live.qcow2 -vga cirrus -vnc :1"
+pkill -f "${QEMUCMD} -hda bluepoint-live.qcow2 -vga cirrus -vnc :1"
 rm bluepoint-live.qcow2
 qemu-img create bluepoint-live.qcow2 -f qcow2 -b bluepoint.qcow2
-qemu-system-i386 -hda bluepoint-live.qcow2 -vga cirrus -vnc :1 &
+$QEMUCMD -hda bluepoint-live.qcow2 -vga cirrus -vnc :1 &
 websockify 45901 localhost:5901 &
